00278d235892ee63883bacc45e0a5313c602a4e3
wendy <menlen6205@gmail.com>
2022-10-17 23:39:52 +0800

    init, 新增取得欄位 mapping 功能

---
+++ src/FileUploader.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import './css/fileUplaoder.css';
+
+const FileUploader = () => {
+  const [mapping, setMapping] = React.useState({});
+  const handleUpload = async (e: any) => {
+    const dataString = (await getDataString(e.target.files[0])) as string;
+
+    setMapping(getDisplayNameAndCodeMappingFromFile(dataString));
+  };
+
+  const getDataString = (file: any) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        resolve(reader.result);
+      });
+      reader.readAsText(file);
+    });
+  };
+
+  const getDisplayName = (line: string) => {
+    const regex = /description = "((?:[^"])*)"/;
+    const [, displayName] = line.match(regex) as Array<string>;
+    return displayName;
+  };
+
+  /**
+   * 配對取出欄位英文名稱
+   * @param line
+   * @returns
+   */
+  const getCode = (line: string) => {
+    const regex = /private .+ (\w+)/;
+    const [, code] = line.match(regex) as Array<string>;
+    return code;
+  };
+
+  /**
+   *
+   * @param fileRoute ex:../Documents/PmsMpAddDetailData.java
+   * @returns
+   */
+  const getDisplayNameAndCodeMappingFromFile = (dataText: string) => {
+    const textByLine = dataText.split('\n');
+    const lines = textByLine.filter(Boolean);
+
+    const displayNameArray = new Array();
+    const codeArray = new Array();
+
+    lines.forEach((line) => {
+      if (line.includes('@Schema')) {
+        const displayName = getDisplayName(line);
+        displayNameArray.push(displayName);
+      }
+      if (line.includes('private')) {
+        const code = getCode(line);
+        codeArray.push(code);
+      }
+    });
+
+    const mapping = Object.assign(
+      {},
+      ...codeArray.map((code, index) => ({ [code]: displayNameArray[index] }))
+    );
+    return mapping;
+  };
+
+  const convertedToCodeBlock = (data: object | string) => {
+    let stringData = data as string;
+    if (typeof data !== 'string') stringData = JSON.stringify(data);
+    return stringData.split(',');
+  };
+
+  return (
+    <>
+      請選擇 openApi .java config 檔案：
+      <input type="file" onChange={handleUpload} />
+      <div className="result__block">
+        轉換結果(欄位中英文 mapping):
+        {convertedToCodeBlock(mapping)?.map((line) => {
+          return <div>{line}</div>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default FileUploader;
